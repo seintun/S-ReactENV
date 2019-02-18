d54e11cbeb4ebe3cb05a3f49c1d42fe5b423e18d
seintun <seintun.edu@gmail.com>
2019-02-17 16:46:05 -0800

    Added loader configurations for js/jsx and css for babel transpilation. Installed css-loader for style rendering

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 const path = require("path");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "/dist"),
-    filename: "index_bundle.js"
+    filename: "index-bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   }
 };
